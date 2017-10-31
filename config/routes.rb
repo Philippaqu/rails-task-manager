ab58959770d42dab2817adfb03dfaecc6dbf2d5e
Philippa Quadt <philippa.quadt@gmail.com>
2017-10-31 15:19:34 +0000

    Task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'task/index'
+
+  # get 'task/show'
+
+  # get 'task/new'
+
+  # get 'task/create'
+
+  # get 'task/edit'
+
+  # get 'task/update'
+
+  # get 'task/destroy'
+
+  resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
